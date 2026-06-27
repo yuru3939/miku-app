64529bab87342f7aa64ca1dde64e1be82c987458
poip116 <zpoipz.sezep@gmail.com>
2026-06-28 01:25:11 +0900

    不要部分の消去

--- js/audio.js
+++ js/audio.js
@@ -15,29 +15,20 @@ let songUrl = "http://www.youtube.com/watch?v=3Wtx6k2vInU"
 const spawn = document.getElementById("spawn-point");
 const SpawnOffsetLeft = spawn.offsetLeft;
 const SpawnOffsetTop = spawn.offsetTop;
-let tes;
 const scoreBoard = document.querySelector("#scoreBoard");
 let score = 0;
-function changetext(unit){
-    if(tes != unit.text){
-        tes = unit.text;
-        
-        return true;
-    }
-    
-    return false;
-    
-}
+
 let progressTime = 1;
 let stt = 0;
-b = document.querySelector("#wordLyric");
+const wordContainer = document.querySelector("#wordLyric");
+//　単語表示
 const animateWord = function (now, unit) {
   if (unit.startTime <= now && unit.endTime > now) {
     if(stt >  musicPosition){
-        console.log("a");
+        console.log("posError");
         return;
     }
-
+    //表示初期化
     if(progressTime > unit.progress(now)){
         console.log("changetext");
         console.log("progressTime:" + progressTime);
@@ -45,13 +36,13 @@ const animateWord = function (now, unit) {
         console.log("text:" + unit.text);
 
         progressTime = unit.progress(now);
-        var nowText = document.createElement('p');
+        let nowText = document.createElement('p');
         nowText.textContent = unit.text;
         nowText.style.left = SpawnOffsetLeft + "px";
         nowText.style.top = SpawnOffsetTop + "px";
         nowText.classList.add("txt");
         nowText.classList.add("fly-lyric");
-        b.appendChild(nowText);
+        wordContainer.appendChild(nowText);
         return;
     }
     progressTime = unit.progress(now);
@@ -59,6 +50,7 @@ const animateWord = function (now, unit) {
 
   
 };
+//　下部フレーズの表示
 const animatePhrase = function (now, unit) {
   if (unit.contains(now)) {
     //console.log(unit.progress(now));
@@ -68,8 +60,8 @@ const animatePhrase = function (now, unit) {
   }
   
 };
-
-b.addEventListener("click", function(event){
+// クリック判定
+wordContainer.addEventListener("click", function(event){
     if(event.target.tagName !== "P") return;
     if(!event.target.classList.contains("touch")){
         event.target.classList.add("touch");
@@ -168,9 +160,9 @@ function run(songName){
             //console.log("a",unit.progress(now));
             //console.log("b",player.timer.position);
 
-            var flyText = document.querySelector(".fly-lyric");
-            var styles = window.getComputedStyle(flyText);
-            var opac = styles.getPropertyValue('opacity');
+            let flyText = document.querySelector(".fly-lyric");
+            let styles = getComputedStyle(flyText);
+            let opac = styles.getPropertyValue('opacity');
             //console.log("opacity",opac);
             if(opac == 0){
                 flyText.remove();
